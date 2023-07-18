5f11a69c3ba587dfc64e444820d1990c4bc05deb
Kevin Gilliam <keving@arizona.edu>
2023-07-18 13:31:06 -0700

    redid circle drawing

--- mirror_view_widget.py
+++ mirror_view_widget.py
@@ -1,13 +1,17 @@
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.properties import ObjectProperty, StringProperty, NumericProperty, BooleanProperty
 from kivy.uix.widget import Widget
 
 class TecWidget(Widget):
     pass
 
-class MirrorViewWidget(RelativeLayout):
+class MirrorWidget(Widget):
     diameter = NumericProperty(100)
-    
+
+class MirrorViewWidget(AnchorLayout):
+    pass
+
 class MirrorViewControlPanel(GridLayout):
     pass
