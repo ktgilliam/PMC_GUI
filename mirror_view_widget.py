5c96c5f40b699c9afce971dba3da26804fa6845f
Kevin Gilliam <keving@arizona.edu>
2023-08-21 14:46:31 -0700

    gui only enables TECs that the box reports having

--- mirror_view_widget.py
+++ mirror_view_widget.py
@@ -11,11 +11,13 @@ from colormap import ColorMap as cmap
 from zernpy import ZernPol
 import math
 
+from tec_iface import *
+
 class TecConfiguration():
     tec_enabled = BooleanProperty()
     tec_power_cmd = NumericProperty()
     tec_temp_diff = NumericProperty()
-
+    
 class TecWidget(Widget):
     id_no = NumericProperty(0)
     spot_diameter = NumericProperty(15)
@@ -32,6 +34,8 @@ class TecWidget(Widget):
     mirror_px_radius = NumericProperty(rebind=True)
     enabled = BooleanProperty(False)
     mag_value = NumericProperty(0, rebind=True)
+    tec_found = BooleanProperty(False)
+    tec_config = None
 
     def connect_mirror_circle(self, mc):
         self.mirror_circle_prop = mc
@@ -63,7 +67,7 @@ class MirrorViewWidget(AnchorLayout):
         super().__init__(**kwargs)
         MirrorViewWidget.instance = self
         
-    def readCsv():
+    def readMirrorConfigCsv():
         root = tk.Tk()
         root.withdraw()
         file_path = filedialog.askopenfilename(
@@ -86,6 +90,44 @@ class MirrorViewWidget(AnchorLayout):
         MirrorViewWidget.cfg_file_path = file_path
         MirrorViewWidget.instance.tec_cfg_list = cfg_list
         MirrorViewWidget.instance.populateTecWidgets()
+
+        
+    def readCommandCsv():
+        root = tk.Tk()
+        root.withdraw()
+        file_path = filedialog.askopenfilename(
+            filetypes=[("CSV File", ".csv")])
+        print(file_path)
+        try:
+            with open(file_path, newline='') as csvfile:
+                spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+                tec_cmds = []
+                headerRow = True
+                for row in spamreader:
+                    if headerRow:
+                        headerRow = False
+                        continue
+                    tec_no = int(row[0])
+                    tec_cmd = float(row[1])
+                    tec_cmd = (tec_no, tec_cmd)
+                    tec_cmds.append(tec_cmd)
+                    print(', '.join(row))
+        except FileNotFoundError:
+            return
+        
+        # for box in TECBoxController._instances:
+        #     list = box.deviceInterface.getTecList()
+        #     for tec in list:
+        #         found = False
+        #         for idx, cmd in enumerate(tec_cmds):
+        #             if cmd[0] == tec.tecNo:
+        #                 print("set tec: " + str(tec.tecNo) + " to " + str(cmd[1]))
+        #                 box.controllerWidget.setFieldValue(tec.tecNo, cmd[1])
+        #                 found = True
+        #                 break
+        #         if found:
+        #             tec_cmds.pop(idx)
+        #     pass
 
     def resetTecWidgets():
         MirrorViewWidget.instance.tec_cfg_list = []
@@ -111,6 +153,7 @@ class MirrorViewWidget(AnchorLayout):
             tw = TecWidget(id_no=cfg[0], theta=cfg[1],rho_norm=rho_norm , enabled=cfg[3])
             tw.connect_mirror_circle(self)
             tw.id = 'TEC'+str(cfg[0])
+            tw.enabled = False
             self.add_widget(tw)
         self.parent.cfg_loaded = True
         
@@ -140,8 +183,15 @@ class MirrorViewWidget(AnchorLayout):
                 centroid_list.append(centroid)
         return centroid_list
 
+    def get_tec_no(self, no):
+        for child in self.children:
+            if type(child) == TecWidget:
+                if child.id_no == no:
+                    return child
+        return None
          
     def zernike_command(self, osa_idx, theta_offs=0.0, scale=1):
+        # Note: This is not that simple - the mag_value will need to come from a surface fit with the influence functions
         theta_offs = theta_offs*math.pi/180
         zp = ZernPol(osa_index=osa_idx)
         cnt = 0
@@ -150,20 +200,32 @@ class MirrorViewWidget(AnchorLayout):
                 theta = (child.theta+theta_offs) % (2*math.pi)
                 Z = zp.polynomial_value(child.rho_norm, theta, use_exact_eq=True)
                 new_mag = 0.5*Z*scale
-                # child.mag_value = Z*scale
                 child.update_mag_value(new_mag)
                 cnt = cnt + 1
         pass
 
-   
+
+    
 class MirrorViewControlPanel(GridLayout):
     active_tec = ObjectProperty(None,  allownone=True)
     opts_disabled = BooleanProperty(True)
     cfg_loaded = BooleanProperty(False)
     
     def setTecEnabledState(self, active):
-        self.active_tec.enabled = active
+        if self.active_tec.tec_found:
+            self.active_tec.enabled = active
         
+    def enable_found_tecs(self, found_tec_cfg_list):
+        mvw = self.ids['mvw']
+        for found_tec_cfg in found_tec_cfg_list:
+            found_tec_no = found_tec_cfg.tecNo
+            tec_widget = mvw.get_tec_no(found_tec_no)
+            if tec_widget is not None:
+                tec_widget.tec_config = found_tec_cfg
+                tec_widget.tec_found = True
+                tec_widget.enabled = True
+            pass
+        pass
         
     def update_tec_color(self, val):
         if self.active_tec is not None:
---
+++ surface_fitting.py
@@ -0,0 +1,4 @@
+
+
+class SurfaceFitter:
+    pass
